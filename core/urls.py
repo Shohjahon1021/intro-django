18308f41ace0602118004fc1a751c6207cbfbdf8
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-21 18:04:23 +0500

    some views

--- core/urls.py
+++ core/urls.py
@@ -5,13 +5,27 @@ from django.http import HttpResponse
 
 
 def home(request):
-    
     return HttpResponse("Hello World")
+
+def about(request):
+    return HttpResponse("About Page")
+
+def contact(request):
+    return HttpResponse("Contact Page")
+
+def blog(request):
+    return HttpResponse("Blog Page")
+
+def projects(request):
+    return HttpResponse("Projects Page")
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', home),
-    # about, contact, blog, projects
+    path('about/', about),
+    path('contact/', contact),
+    path('blog/', blog),
+    path('projects/', projects),
 ]
