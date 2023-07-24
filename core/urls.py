10469d82e630e2e3be98ace0e5007a2881b71301
Shohjahon1021 <shohjahonberdimurotov21@gmail.com>
2023-07-24 17:29:42 +0500

    include

--- core/urls.py
+++ core/urls.py
@@ -1,25 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-
-from django.http import HttpResponse, HttpRequest
-
-
-
-from myapp.views import home, about, contact, blog, projects, sum
-
-def hi(request, name):
-    return HttpResponse(f"Hello {name}!")
-
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', home),
-    path('about/', about),
-    path('contact/', contact),
-    path('blog/', blog),
-    path('projects/', projects),
-
-    path('sum', sum),
-    path('hi/<str:name>', hi)
+    path('api/',include('myapp.urls'))
 ]
+
